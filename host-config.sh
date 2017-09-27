4b0b4cd6f2de8cdb88b09ef1797255bf8fca8b18
made2001 <matthieudechamps@hotmail.com>
2017-09-27 14:42:48 +0200

    Update host-config.sh

--- host-config.sh
+++ host-config.sh
@@ -2,14 +2,13 @@
 
 HOSTNAME=$1
 
-#echo $HOSTNAME > /etc/hostname
 
 echo "127.0.0.1       localhost" > /etc/hosts
-echo "127.0.1.1       $1.dechamps   $1" >> /etc/hosts
+echo "127.0.1.1       $1.YOURDOMAINNAME   $1" >> /etc/hosts
 echo "# The following lines are desirable for IPv6 capable hosts" >> /etc/hosts
 echo "::1     localhost ip6-localhost ip6-loopback" >> /etc/hosts
 echo "ff02::1 ip6-allnodes" >> /etc/hosts
 echo "ff02::2 ip6-allrouters" >> /etc/hosts
 
 
-rm -rf /root/hosts-config.sh
+rm -f /root/hosts-config.sh
